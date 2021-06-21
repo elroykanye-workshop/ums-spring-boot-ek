71e327582b33b3a06f6e96a826cc17fe3c82b613
elroykanye <elroykanye@gmail.com>
2021-06-21 15:14:54 +0100

    main applition class

---
+++ src/main/java/com/elroykanye/userregsystem/UserRegSystemApplication.java
@@ -0,0 +1,13 @@
+package com.elroykanye.userregsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserRegSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/elroykanye/userregsystem/UserRegSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elroykanye.userregsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
