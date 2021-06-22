2013f6fe379a69dbd293c95a6b62a127d03a785a
elroykanye <elroykanye@gmail.com>
2021-06-22 01:57:35 +0100

    added request handlers for create, update, delete

--- src/main/java/com/elroykanye/userregsystem/rest/UserRegRestController.java
+++ src/main/java/com/elroykanye/userregsystem/rest/UserRegRestController.java
@@ -6,10 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
