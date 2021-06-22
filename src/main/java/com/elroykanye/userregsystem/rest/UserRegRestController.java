d531ab01638fc30826f98c4136e8ea100228033f
elroykanye <elroykanye@gmail.com>
2021-06-22 01:57:21 +0100

    added request handlers for create, update, delete

--- src/main/java/com/elroykanye/userregsystem/rest/UserRegRestController.java
+++ src/main/java/com/elroykanye/userregsystem/rest/UserRegRestController.java
@@ -28,4 +28,39 @@ public class UserRegRestController {
         return new ResponseEntity<>(users, HttpStatus.OK);
     }
 
+    @PostMapping(value="/", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO> createUser(@RequestBody final UserDTO user) {
+        userJpaRep.save(user);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{user_id}")
+    public ResponseEntity<Optional<UserDTO>> getUserById(@PathVariable("user_id") final Long id) {
+        Optional<UserDTO> user = userJpaRep.findById(id);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/{user_id}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Optional<UserDTO>> updateUser(@PathVariable("user_id") final Long id, @RequestBody UserDTO user) {
+        // fetch a user based on id and set it to currentUser object of type DTO
+        Optional<UserDTO> currUser = userJpaRep.findById(id);
+
+        // update currUser object data with user object data
+        currUser.get().setName(user.getName());
+        currUser.get().setAddress(user.getAddress());
+        currUser.get().setEmail(user.getEmail());
+
+        // save the current user object
+        userJpaRep.saveAndFlush(currUser.get());
+
+        // return a response entity object
+        return new ResponseEntity<>(currUser, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{user_id}")
+    public ResponseEntity<UserDTO> deleteUser(@PathVariable("user_id") final Long id) {
+        userJpaRep.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
 }
